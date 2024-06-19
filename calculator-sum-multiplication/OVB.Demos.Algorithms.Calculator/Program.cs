74ff81b980aee8aa6a7556eb1ffbd81e95a27a13
OtavioVB <otaviovb.developer@gmail.com>
2024-06-19 20:41:53 -0300

    feature: division rest

---
+++ calculator-sum-multiplication/OVB.Demos.Algorithms.Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OVB.Demos.Algorithms.Calculator;
+
+public sealed class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static (int SumResult, int MultiplicationResult) Operation(int x, int y)
+        => (x + y, x * y);
+}
---
+++ division-rest-algorithm/OVB.Demos.Algorithms.DivisionRest/DivisionRest.cs
@@ -0,0 +1,36 @@
+﻿namespace OVB.Demos.Algorithms.DivisionRest;
+
+public sealed class Program
+{
+    public int Mod(int x, int y)
+        => x % y;
+
+    public bool IsOdd(int number)
+        => Mod(number, 2) == 1;
+
+    /// <summary>
+    /// O(n/4)
+    /// </summary>
+    /// <returns></returns>
+    public int CalculateSumOfOddNumbers()
+    {
+        var sumOfOddNumbers = 0;
+
+        var startCursor = 101;
+        var endCursor = 199;
+
+        while ((startCursor - endCursor) != 0)
+        {
+            if (IsOdd(startCursor))
+                sumOfOddNumbers += startCursor;
+
+            if (IsOdd(endCursor))
+                sumOfOddNumbers += endCursor;
+
+            endCursor -= 2;
+            startCursor -= 2;
+        }
+
+        return sumOfOddNumbers;
+    }
+}
